dedd3faceb1edc6c6c4f694b7db1cca450eae0a1
Mohamad Ibrahim <mohamedibr@gmail.com>
2021-09-10 16:55:27 +0200

    test from intelij terminal

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("I am a git project");
     System.out.println("I am a test branch");
+    System.out.println("I am another testbranch but from intellij terminal");
+
   }
 }
